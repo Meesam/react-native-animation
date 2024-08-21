39010858752bd4ab1c7fa88ab6465b5774d56af6
Meesam <meesam.engineer@gmail.com>
2024-08-21 19:49:45 +0530

    added tanstack query

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,18 +1,54 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
+
+interface article {
+  source: {
+    id: string;
+    name: string;
+  };
+  author: string;
+  title: string;
+  description: string;
+  url: string;
+  urlToImage: string | null;
+  publishedAt: string;
+  content: string;
+}
+interface newsApiResponse {
+  status: string;
+  totalResults: number;
+  articles: article[];
+}
+
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const Profile = () => {
-  const getNews = async () => {
-    fetch(
+  const getNews = async (): Promise<newsApiResponse> => {
+    const result = await fetch(
       "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a369c7c7aacb4f92a125378645453c98"
-    ).then((res: any) => {
-      console.log("res ", res);
-    });
+    );
+    return result.json();
   };
+
+  const { isPending, isError, data, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: getNews,
+  });
 
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <FlatList
+        data={data?.articles}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={(item) => item.url}
+      />
     </View>
   );
 };
@@ -24,5 +60,15 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 10,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
 import { Stack } from "expo-router";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
