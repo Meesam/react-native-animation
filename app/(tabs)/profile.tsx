5256a137807be87cb7a47c93d281ad9b71b8728c
Meesam Zaidi <meesam.engineer@gmail.com>
2024-08-22 10:03:08 +0530

    work on flat list

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
@@ -21,18 +21,28 @@ interface newsApiResponse {
   articles: article[];
 }
 
-type ItemProps = { title: string };
+type ItemProps = { title: string; urlToImage: string | null };
 
-const Item = ({ title }: ItemProps) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
+const Item = ({ title, urlToImage }: ItemProps) => (
+  <View style={styles.itemContainer}>
+    <Image
+      source={{
+        uri: urlToImage
+          ? urlToImage
+          : "https://unsplash.com/photos/lemon-water-in-footed-glass-RgplfXbxLFs",
+      }}
+      style={styles.itemImage}
+    />
+    <View style={styles.itemText}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
   </View>
 );
 
 const Profile = () => {
   const getNews = async (): Promise<newsApiResponse> => {
     const result = await fetch(
-      "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a369c7c7aacb4f92a125378645453c98"
+      "https://newsapi.org/v2/everything?q=bitcoin&apiKey=a369c7c7aacb4f92a125378645453c98"
     );
     return result.json();
   };
@@ -46,8 +56,12 @@ const Profile = () => {
     <View style={styles.container}>
       <FlatList
         data={data?.articles}
-        renderItem={({ item }) => <Item title={item.title} />}
+        renderItem={({ item }) => (
+          <Item title={item.title} urlToImage={item.urlToImage} />
+        )}
         keyExtractor={(item) => item.url}
+        ListHeaderComponent=<Text>Hello Header</Text>
+        ListFooterComponent=<Text>Hello Footer</Text>
       />
     </View>
   );
@@ -58,17 +72,24 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
     marginTop: 10,
   },
-  item: {
+  itemContainer: {
     backgroundColor: "#f9c2ff",
-    padding: 20,
+    padding: 10,
     marginVertical: 8,
-    marginHorizontal: 16,
+    flexDirection: "row",
+    gap: 10,
+  },
+  itemImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  itemText: {
+    width: 320,
   },
   title: {
-    fontSize: 32,
+    fontSize: 16,
   },
 });
